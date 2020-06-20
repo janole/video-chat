069e3f40be50699402ec778578b3ef64340df2b8
Jan Ole Suhr <ole@mobileways.de>
2020-06-20 19:06:53 +0200

    Remove unused code.

--- frontend/src/utils/MediaUtils.js
+++ frontend/src/utils/MediaUtils.js
@@ -1,24 +1,6 @@
 
 import 'webrtc-adapter';
 
-/*
-function enumerateDevices()
-{
-    navigator.mediaDevices.enumerateDevices()
-        .then(devices =>
-        {
-            console.log("devices", devices);
-            let videoInputDevices = 0;
-            for (const device of devices)
-            {
-                if (device.kind === "videoinput")
-                    videoInputDevices++;
-            }
-            this.setState({ videoInputDevices: videoInputDevices });
-        });
-}
-*/
-
 function getStream(facingMode = "user", width = 640, height = 480)
 {
     return new Promise((resolve, reject) =>
