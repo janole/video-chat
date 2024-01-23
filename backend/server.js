f9115bda93de8d27b5b29ab8d073a76758b7423b
janole <ole@mobileways.de>
2024-01-23 14:02:24 +0100

    Upgrade server to socket.io 4

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,13 @@
 //
 const app = require("express")();
 const server = require("http").Server(app);
-const io = require("socket.io")(server, { path: "/", cookie: false });
+const io = require("socket.io")(server, { path: "/", cookie: false, cors: { origin: "*" } });
 
 //
 const peerConfig = require("./peer-config");
 
 //
 const port = process.env.LISTEN_PORT || 4999;
-
-// Enable CORS
-io.set("origins", "*:*");
 
 //
 server.listen(port);
@@ -19,40 +16,38 @@ server.listen(port);
 console.log("* Server started on " + port);
 
 //
-io.on("connection", function (socket)
+io.on("connection", (socket) =>
 {
+    socket.username = socket.handshake.auth.username;
+
     socket.on("enter", (data) =>
     {
         socket.join(data.roomId);
         socket.roomId = data.roomId;
 
-        const room = io.of("/").in().adapter.rooms[socket.roomId];
-        const config = peerConfig.get(data.roomId, process.env.TURN_SECRET);
+        io.in(data.roomId).fetchSockets().then(sockets =>
+        {
+            const config = peerConfig.get(data.roomId, process.env.TURN_SECRET);
 
-        socket.emit("sockets", {
-            sockets: room.sockets,
-            peerConfig: config,
+            console.log("enter", socket.roomId);
+
+            socket.emit("sockets", {
+                sockets: sockets.map(s => s.username),
+                peerConfig: config,
+            });
         });
-
-        console.log("enter", socket.roomId, room, config);
     });
 
     socket.on("disconnect", () =>
     {
-        const sockets = io.of("/").in().adapter.rooms[socket.roomId];
-
-        console.log("disconnect", socket.id, socket.roomId, sockets);
-
         if (socket.roomId)
         {
-            socket.to(socket.roomId).emit("message", { from: socket.id, type: "disconnected" });
+            socket.to(socket.roomId).emit("message", { from: socket.username, type: "disconnected" });
         }
     });
 
     socket.on("signal", (data) =>
     {
-        console.log("signal", socket.id, data.room);
-
         socket.to(data.room).emit("signal", data);
     });
 
